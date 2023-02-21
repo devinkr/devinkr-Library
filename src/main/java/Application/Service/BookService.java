902ccf1f168aeb9a0e6d55130bdc9e704de7ee03
Devin Raleigh <devinkr@gmail.com>
2023-02-21 21:39:44 +0000

    Complete Library

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -40,7 +40,8 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return null;
+        List<Book> books = bookDAO.getAllBooks();
+        return books;
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,15 +52,20 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-
-        return null;
+        Book bookExists = bookDAO.getBookByIsbn(book.getIsbn());
+        if (bookExists != null) {
+            return null;
+        }
+        Book insertedBook = bookDAO.insertBook(book);
+        return insertedBook;
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return null;
+        List<Book> books = bookDAO.getBooksWithBookCountOverZero();
+        return books;
     }
 
 }
